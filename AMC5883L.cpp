21953ed6ea967caeb5dccfc4f4a01dd26976bea3
L-eeX <1035868977@qq.com>
2018-03-22 17:39:44 +0800

    compass

--- AMC5883L.cpp
+++ AMC5883L.cpp
@@ -4,8 +4,8 @@ using namespace pxt;
 
 namespace AMC5883L {
     //%
-    StringData* cpp_division(int size){
-        int n = size;
+    StringData* cpp_division(int x, int y){
+        int n = 2;
         if (n == 0) return ManagedString("").leakData();
         return ManagedString(uBit.serial.read(n, MicroBitSerialMode::ASYNC)).leakData();
     }
