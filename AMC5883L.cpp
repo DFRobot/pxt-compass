c8a57927a725f7ef34fa69212de0a7fb99d7cf19
L-eeX <1035868977@qq.com>
2018-03-22 17:10:32 +0800

    compass

--- AMC5883L.cpp
+++ AMC5883L.cpp
@@ -5,13 +5,13 @@ using namespace pxt;
 namespace AMC5883L {
 
     //%
-    Buffer cpp_createBuffer(int size)
+    ManagedBuffer cpp_createBuffer(int size)
     {
         return ManagedBuffer(size).leakData();
     }
 
     //%
-    Buffer cpp_division(int a, int b){
+    ManagedBuffer cpp_division(int a, int b){
         Buffer buf = cpp_createBuffer(2);
         buf[0] = a / b;
         buf[0] = a % b;
@@ -19,3 +19,4 @@ namespace AMC5883L {
     }
 
 }
+
